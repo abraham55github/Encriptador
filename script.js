ee569d07a27988a52cc6165bf3fc925a549e0a76
abraham55github <155010549+abraham55github@users.noreply.github.com>
2024-03-16 17:34:52 -0500

    Añadimos la utilizacion del boton desencriptar modifique el script

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ botonEncriptar.addEventListener('click', function(){
     encriptarTexto();
 })
 
+botonDesencriptar.addEventListener('click', function(){
+    desencriptarTexto();
+})
+
 function encriptarTexto(){
     const texto = document.querySelector('#texto').value;
     const tituloMensaje = document.querySelector('.titulo-mensaje')
@@ -19,13 +23,33 @@ function encriptarTexto(){
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
 
-
     if (texto.length !== 0) {
         noImage();
         tituloMensaje.textContent = "Texto cifrado correctamente";
         parrafo.textContent = textoCifrado;
     }
 
+
+}
+
+function desencriptarTexto(){
+    const texto = document.querySelector('#texto').value;
+    const tituloMensaje = document.querySelector('.titulo-mensaje')
+    const parrafo = document.querySelector('.parrafo')
+
+
+    let textoDecifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if (texto.length !== 0) {
+        noImage();
+        tituloMensaje.textContent = "Texto decifrado correctamente";
+        parrafo.textContent = textoDecifrado;
+    }
 
 }
 
